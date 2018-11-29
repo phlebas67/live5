d639811e9f7bd80a4c46daf30f61db30ef8a65e8
phlebas67 <colin.terry@sky.com>
2018-11-29 17:25:34 +0000

    Initial test of node module completed

--- node/server.js
+++ node/server.js
@@ -1,12 +1,44 @@
-/*eslint no-console: 0*/
+/*eslint no-console: 0, no-unused-vars: 0, no-undef:0*/
 "use strict";
 
-var http = require("http");
+var xsenv = require("@sap/xsenv");
+var passport = require("passport");
+var xssec = require("@sap/xssec");
+var xsHDBConn = require("sap-hdbext");
+var express = require("express");
+var logging = require("sap-logging");
+var async = require("async");
+
 var port = process.env.PORT || 3000;
+var server = require("http").createServer();
+var appContext = logging.createAppContext();
+var app = express();
 
-http.createServer(function (req, res) {
-  res.writeHead(200, {"Content-Type": "text/plain"});
-  res.end("Hello World\n");
-}).listen(port);
+passport.use("JWT", new xssec.JWTStrategy(xsenv.getServices({
+	uaa: {
+		tag: "xsuaa"
+	}
+}).uaa));
 
-console.log("Server listening on port %d", port);
+app.use(logging.expressMiddleware(appContext));
+app.use(passport.initialize());
+
+var hanaOptions = xsenv.getServices({
+	hana: {
+		tag: "hana"
+	}
+});
+
+app.use(
+	passport.authenticate("JWT", {
+		session: false
+	}),
+	xsHDBConn.middleware(hanaOptions.hana)
+);
+
+var router = require("./router")(app, server);
+
+server.on("request", app);
+server.listen(port, function() {
+	console.info("HTTP Server listening on port: " + server.address().port);
+});
